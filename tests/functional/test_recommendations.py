585fcee26a817b20a716fa0e50b7ac3d5b639141
Prayson W. Daniel <pradan@norlys.dk>
2025-02-22 19:26:46 +0100

    adds step3 pytest's xfail to reschue

--- tests/functional/test_recommendations.py
+++ tests/functional/test_recommendations.py
@@ -2,6 +2,8 @@ from typing import TypedDict
 
 import pytest
 
+from beacon import recommeder
+
 
 class Request(TypedDict):
     query: str
@@ -13,6 +15,20 @@ USER_STORIES: list[Request] = [
     {"query": "a lawyer driving a fancy car", "expects": "The Lincoln Lawyer"},
     {"query": "a boy going to a magic school of wizards", "expects": "Harry Potter"},
 ]
+
+
+@pytest.mark.xfail(reason="show cases tdd done right")
+@pytest.mark.parametrize("story", USER_STORIES, ids=["twilight", "lincoln", "potter"])
+def test_becons(story):
+    # Arrange
+    query = story.get("query")
+    expected = story.get("expects")
+
+    # Act
+    recommendations = recommeder.recommend(query)
+
+    # Assert
+    assert any(expected in recommendation for recommendation in recommendations)
 
 
 def test_fail():
